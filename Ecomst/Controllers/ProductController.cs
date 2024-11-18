f62cefdddae71e4df7ee0dfbcfa87253c77e6664
AnakinFranzela <v.d.yanchev@gmail.com>
2024-11-18 09:38:10 +0200

    Add project files.

---
+++ Ecomst/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Ecomst.DTO;
+using Ecomst.Entities;
+using Ecomst.Services.IServices;
+using Ecomst.ViewModels.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecomst.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index(ProductTableViewModel viewModel)
+        {
+            ProductSearch searchModel = new ProductSearch();
+            searchModel.CategoryId = viewModel.CategoryId;
+            searchModel.CodeNumber = viewModel.CodeNumber;
+            searchModel.Title = viewModel.Title;
+            searchModel.Description = viewModel.Description;
+            searchModel.InStock = viewModel.InStock;
+            SearchResult<Product> result = _productService.Search(searchModel, viewModel.SortOrder, viewModel.PageNumber, viewModel.Length);
+            viewModel.PopulateFromSearchResult(result);
+            return View(viewModel);
+        }
+    }
+}
---
+++ Ecomst/ViewModels/Product/ProductTableViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Ecomst.DTO;
+using Ecomst.Helpers;
+using System.ComponentModel;
+
+namespace Ecomst.ViewModels.Product
+{
+    public class ProductTableViewModel:BaseTableViewModel
+    {
+        public List<Ecomst.Entities.Product> Data { get; set; }
+        public void PopulateFromSearchResult(SearchResult<Ecomst.Entities.Product> searchResult)
+        {
+            TotalPages = searchResult.TotalPages;
+            RecordsTotal = searchResult.RecordsTotal;
+            RecordsFiltered = searchResult.RecordsFiltered;
+            Start = searchResult.Start;
+            Data = searchResult.Data;
+        }
+
+        //Search properties
+        [DisplayName("Категория")]
+        public int CategoryId { get; set; }
+        [DisplayName("Номер на продукта")]
+        public string CodeNumber { get; set; }
+        [DisplayName("Заглавие")]
+        public string Title { get; set; }
+        [DisplayName("Описание")]
+        public string Description { get; set; }
+        [DisplayName("В наличност")]
+        public bool InStock { get; set; }
+        [DisplayName("Цена")]
+        public decimal Price { get; set; }
+    }
+}
